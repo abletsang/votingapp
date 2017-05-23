d8c3bc3ac2a0e84caa7a60039f3490421a4eada7
Able Tsang <abletsang@gmail.com>
2017-05-22 21:31:59 -0700

    do log in and log out routes

--- routes/index.js
+++ routes/index.js
@@ -1,16 +1,21 @@
 var express = require("express"),
 	router = express.Router(),
 	passport = require("passport"),
-	User = require("../models/user");
+	User = require("../models/user"),
+	Poll = require("../models/polls");
 
+// root route ======================================================================
 router.get("/", function(req, res) {
 	res.render("landingpage");
 });
 
+// register new user form ===========================================================
 router.get("/register", function(req, res) {
 	res.render("register");
 });
 
+
+// register new user ===============================================================
 router.post("/register", function(req, res) {
 	User.register(new User({username: req.body.username}), req.body.password, function(err, user) {
 		if (err) {
@@ -21,6 +26,24 @@ router.post("/register", function(req, res) {
 			res.redirect("/");
 		});
 	});
+});
+
+// log in form =====================================================================
+router.get("/login", function(req, res) {
+	res.render("login");
+});
+
+// log in post request =============================================================
+router.post("/login", passport.authenticate("local", {
+	successRedirect: "/polls",
+	failureRedirect: "/login"
+	}), function(req, res) {
+});
+
+// log out =========================================================================
+router.get("/logout", function(req, res) {
+	req.logout();
+	res.redirect("/");
 });
 
 module.exports = router;
