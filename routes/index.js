0cb24a9e9d9751ffdb396f59e0ae64f587119e1d
Able Tsang <abletsang@gmail.com>
2017-05-22 17:02:30 -0700

    require User model, set up POST route for registering users

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,7 @@
 var express = require("express"),
 	router = express.Router(),
-	passport = require("passport");
-
-console.log("index.js");
+	passport = require("passport"),
+	User = require("../models/user");
 
 router.get("/", function(req, res) {
 	res.render("landingpage");
@@ -10,6 +9,18 @@ router.get("/", function(req, res) {
 
 router.get("/register", function(req, res) {
 	res.render("register");
+});
+
+router.post("/register", function(req, res) {
+	User.register(new User({username: req.body.username}), req.body.password, function(err, user) {
+		if (err) {
+			console.log(err);
+			return res.render("register");
+		}
+		passport.authenticate("local")(req, res, function() {
+			res.redirect("/");
+		});
+	});
 });
 
 module.exports = router;
