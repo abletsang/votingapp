63e57a5ac3dace6d4b4a2ee5dbaafe560f462fc9
Able Tsang <abletsang@gmail.com>
2017-05-22 22:09:32 -0700

    have the register and log inlinks in the navbar show up when a user is not logged in, and a "signed in as <username>" and account dropdown show up when a user is logged in
    (currentUser was undefined and could not recognize that a user had logged in until after changing the order that packages and routes were required/used in app.js)

--- app.js
+++ app.js
@@ -1,13 +1,20 @@
 var express = require("express"),
+	app = express(),
 	mongoose = require("mongoose"),
 	bodyParser = require("body-parser"),
 	passport = require("passport"),
 	LocalStrategy = require("passport-local"),
 	methodOverride = require("method-override"),
+	passportLocalMongoose = require("passport-local-mongoose"),// ??? wasn't in yelpcamp app.js
 	User = require("./models/user"),
 	Poll = require("./models/polls");
 
-var app = express();
+// require routes ========================================================================
+var indexRoutes = require("./routes/index");
+var pollRoutes = require("./routes/polls");
+
+// use packages and database ===========================================================
+
 mongoose.connect("mongodb://localhost/votingapp");
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
@@ -15,11 +22,6 @@ app.use(express.static(__dirname + "/pulic"));
 app.use(methodOverride("_method"));
 
 // configure passport ====================================================================
-app.use(function(req, res, next) {
-	res.locals.currentUser = req.user;
-	next();
-});
-
 app.use(require("express-session")({
 	secret: "This is my first backend project!",
 	resave: false,
@@ -32,11 +34,12 @@ passport.use(new LocalStrategy(User.authenticate()));
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
 
-// require/link routes ========================================================================
-var indexRoutes = require("./routes/index");
-var pollRoutes = require("./routes/polls");
+app.use(function(req, res, next) {
+	res.locals.currentUser = req.user;
+	next();
+});
 
-
+// linking to routes ====================================================================
 app.use(indexRoutes);
 app.use(pollRoutes);
 
